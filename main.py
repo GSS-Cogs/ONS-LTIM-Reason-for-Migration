00442e4c9f2300224e4f5ae16c67bdbf4d871848
RedWalters <jonathan.j.walters@Gmail.com>
2019-10-14 11:55:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -73,6 +73,7 @@ df['Year'] = df.apply(lambda x: int(float(x['Year'])), axis = 1)
 df['Reason for Migration'] = df.apply(lambda x: x['Reason for Migration'][:-1] if x['Reason for Migration'].endswith('2') else x['Reason for Migration'], axis = 1)
 df['Reason for Migration'] = df.apply(lambda x: x['Reason for Migration'] if x['Reason2'] == '' else x['Reason for Migration'] + ' - ' + x['Reason2'], axis = 1)
 df['Reason for Migration'] = df.apply(lambda x: x['Reason for Migration'][:-1] if x['Reason for Migration'].endswith('1') else x['Reason for Migration'], axis = 1)
+df['CI'] = df.apply(lambda x: left(x['CI'], len(str(x['CI'])) - 2) if x['CI'].endswith('.0') else x['CI'], axis = 1)
 df = df.drop(['Reason2'], axis = 1)
 df.rename(columns={'OBS':'Value',
                    'DATAMARKER':'Marker'}, 
